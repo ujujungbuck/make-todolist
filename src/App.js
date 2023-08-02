0a7b328e6d30a90dd3d40593af8d54f9c21a2cb4
ujujungbuck <garam8u2@gmail.com>
2023-08-02 17:10:00 +0900

    Feat : initial Setiing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          todoApp만들어보자 
-        </p>
+        <p>todoApp만들어보자 !</p>
         <a
           className="App-link"
           href="https://reactjs.org"
