7827e7130ae668d0247b1df11beaf21a8a9c1c2f
ujujungbuck <garam8u2@gmail.com>
2023-08-06 15:54:58 +0900

    Feat : onInsert,onRemove,onToggle생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
+import { useState, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '오빠 공항 픽업',
+      checked: false,
+    },
+    {
+      id: 2,
+      text: '리액트 10장 실습',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '코테 풀기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -3,7 +3,7 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정 관리</div>
+      <div className="app-title">To do for today</div>
       <div className="content">{children}</div>
     </div>
   );
