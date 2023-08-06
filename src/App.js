1dd40b36023af030b4a5ae18e2963e6c1043b3ea
ujujungbuck <garam8u2@gmail.com>
2023-08-06 14:09:15 +0900

    Feat : Todolist작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
